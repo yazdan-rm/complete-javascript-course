ca3ee8d4749a9e718dfcf0268b4b79f91bcc5c06
Yazdan <1yazdanrafieemanesh@gmail.com>
2023-09-29 15:15:20 +0330

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,19 @@
+let country = "Iran";
+let continent = "miduless";
+let population = 85;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "jonas");
+
+country = 123;
+console.log(country);
+
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
+console.log(isIsland);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = "amazing ";
+console.log(40 + 12 + 34);
+
+let firstName = "Yazdan ";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+alert("Hello yazdan To the world of programming ");
+
+let lastName = "RafieeManesh";
+console.log(lastName);
