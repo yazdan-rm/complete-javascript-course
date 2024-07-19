a5d562453a6d1c476dad7f662773af42a95494cd
Yazdan <1yazdanrafieemanesh@gmail.com>
2024-07-19 16:25:42 +0330

    advance dom traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -41,3 +41,41 @@ document.addEventListener('keydown', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+
+// Selecting an element
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+
+const header = document.querySelector('.header');
+const allSection = document.querySelectorAll('.section');
+console.log(allSection);
+
+document.getElementById('section--1');
+const allButton = document.getElementsByTagName('button');
+console.log(allButton);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting elements
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics.<button class="btn btn--close-cookies">got it!</button>';
+
+
+// header.prepend(message);
+header.append(message);
+
+// header.before(message);
+// header.after(message);
+
+// Deleting elements
+document
+  .querySelector('.btn--close-cookies')
+  // .addEventListener('click', ()=> message.remove());
+  .addEventListener('click', (e)=>{
+    message.parentElement.removeChild(message);
+  });
