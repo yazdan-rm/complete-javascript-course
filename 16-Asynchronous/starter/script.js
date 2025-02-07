8cc78cffc774fb5e9fc810bbc9a67598fb5ae35f
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-07 23:36:32 +0330

    middle of AJAX call promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,124 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// const getCountriesData = function(country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country.toLowerCase()}`, true);
+//   request.send();
+//   request.addEventListener('load', function() {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//
+//     const html = `
+//           <article class="country">
+//           <img class="country__img" src="${data.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages.fas}</p>
+//           </div>
+//         </article>
+//   `;
+//
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+//
+// getCountriesData('usa');
+// getCountriesData('iran');
+// getCountriesData('germany');
+
+const renderCountry = function(data, className = '') {
+  const html = `
+          <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.fas}</p>
+          </div>
+        </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryAndNeighbour = function(country) {
+//
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country.toLowerCase()}`, true);
+//   request.send();
+//
+//   request.addEventListener('load', function() {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//
+//     // Render country 1
+//     renderCountry(data);
+//
+//     // Get neighbour country 2
+//     const neighbour = data.borders?.[0];
+//
+//     if(!neighbour) return;
+//     // AJAX call country 1
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`, true);
+//     request2.send();
+//
+//     request2.addEventListener('load', function() {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+//
+//       // Render country 2
+//       renderCountry(data2, 'neighbour');
+//     })
+//   });
+// };
+//
+// getCountryAndNeighbour('iran');
+// // Callback hell
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000)
+//     }, 1000)
+//   }, 1000)
+// }, 1000);
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${coduntry.toLowerCase()}`, true);
+// request.send();
+
+const request = fetch('https://restcountries.com/v3.1/name/iran');
+console.log(request);
+
+const getCountryData = function(country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(request => request.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      if(!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+  .then(data => renderCountry(data[0]));
+};
+
+getCountryData('iran');
+
+
